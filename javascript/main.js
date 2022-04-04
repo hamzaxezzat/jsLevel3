f3a4a6e3d57d21d37ec5eabf10ab75059ffa6783
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-04-04 19:52:08 +0200

    Random Header Color

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-
+ 
 // Toggale the Light and dark mode
 {const dark = document.getElementById("dark");
 const body = document.getElementById("body");
@@ -19,3 +19,21 @@ changecolor.addEventListener('mouseout', (eo) =>{
   hiddenul.style.display = "none";
 });}
 
+
+
+// Change Header BG Color
+const header = document.getElementsByTagName("header")[0];
+const h1Title= document.querySelector("body > header > h1")
+headerbg.addEventListener("click",(eo)=>{
+  const random = Math.round(Math.random() *360);
+  header.style.backgroundColor = `hsl(${random}, 50%,50%)`;
+  h1Title.style.backgroundColor = `hsl(${random}, 50%,50%)`;
+  h1Title.style.backgroundColor = `hsl(${random}, 50%,50%)`;
+  dark.style.backgroundColor = `hsl(${random}, 80%,50%)`;
+})
+/*
+// =============== Advanced Selectors ===============
+document.querySelectorAll("h1:not(body > header > h1 )");
+document.querySelectorAll("h1:not(body > header > h1 )");
+// Copy Selector from Inspect Element
+*/
