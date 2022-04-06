fbaa48cc05c51c46d159bca6e6dcb4559227b280
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-04-06 13:44:11 +0200

    fallHeart

--- javascript/randomHearts.js
+++ javascript/randomHearts.js
@@ -1,22 +1,32 @@
 const btnRandomE = document.querySelector(".btnRandomE");
-// const heart = document.querySelector(".heart");
 const emojisParent = document.querySelector(".emojisParent");
-console.log(`${Math.random()*100}%`);
-const fall = btnRandomE.addEventListener("click",eo=>{
-  
+
+const fallHearts = ()=>{
   const fallInterval = setInterval(() => {
 
     const heart = document.createElement("div");
     heart.classList.add("heart");
-    heart.innerHTML = "&#128157;";
-
+    heart.innerHTML = "&#128156;";
     heart.style.left  = `${Math.random() * 100 }%`;
-    emojisParent.append(heart);},50);
+    heart.style.animationDuration = `${(Math.random()+.5)*1.5}s`;
 
+    const heart2 = document.createElement("div");
+    heart2.classList.add("heart");
+    heart2.innerHTML = "&#128150;";
+    heart2.style.left  = `${Math.random() * 100 }%`;
+    heart2.style.animationDuration = `${(Math.random()+.5)*1.5}s`;
+
+    emojisParent.append(heart);
+    emojisParent.append(heart2)
+    }
+    ,50);
+    // Stop after 3s
     setTimeout(() => {
       clearInterval(fallInterval);
-    }, 3000);
+    }, 1000);
+    // clear the div after 5s
     setTimeout(() => {
       emojisParent.innerText = "";
-    }, 5000);
-  });
+    }, 3000);
+}
+btnRandomE.addEventListener("click",eo=>{fallHearts()});
